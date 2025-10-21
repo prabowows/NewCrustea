e0af464e2c4aeb7a34194a1cd1fd045a0fb68049
Prabowo Wahyu Sudarno <PRABOWOWahyu45@gmail.com>
2025-10-21 04:31:14 +0000

    Atas ini tolong ditambah lokasi ponds, pindahkan dari yang existing (_fo

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -8,16 +8,18 @@ import { LocationOverview } from "@/components/dashboard/location-overview";
 
 export default function DashboardPage() {
   return (
-    <main className="grid flex-1 items-start gap-4 p-4 sm:px-6 sm:py-0 md:gap-8 lg:grid-cols-3 xl:grid-cols-3">
-      <div className="grid auto-rows-max items-start gap-4 md:gap-8 lg:col-span-2">
-        <RealTimeMetrics />
-        <HistoricalChart />
-        <AlarmsTable />
-        <ParameterAnalysis />
-      </div>
-      <div className="grid auto-rows-max items-start gap-4 md:gap-8 lg:col-span-1">
-        <LocationOverview />
-        <AeratorControl />
+    <main className="flex-1 space-y-4 p-4 sm:px-6 sm:py-0 md:space-y-8">
+      <LocationOverview />
+      <div className="grid grid-cols-1 gap-4 md:gap-8 lg:grid-cols-3 xl:grid-cols-3">
+        <div className="grid auto-rows-max items-start gap-4 md:gap-8 lg:col-span-2">
+          <RealTimeMetrics />
+          <HistoricalChart />
+          <AlarmsTable />
+          <ParameterAnalysis />
+        </div>
+        <div className="grid auto-rows-max items-start gap-4 md:gap-8 lg:col-span-1">
+          <AeratorControl />
+        </div>
       </div>
     </main>
   );
