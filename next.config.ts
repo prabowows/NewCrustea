23e5a07346aa35da7ea62ef89d850d4289fe6568
Prabowo Wahyu Sudarno <PRABOWOWahyu45@gmail.com>
2025-10-21 02:31:41 +0000

    Try fixing this error: `Runtime Error: Error: Invalid src prop (https://

--- next.config.ts
+++ next.config.ts
@@ -33,6 +33,12 @@ const nextConfig: NextConfig = {
         hostname: 'res.cloudinary.com',
         port: '',
         pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'crustea.id',
+        port: '',
+        pathname: '/**',
       }
     ],
   },
