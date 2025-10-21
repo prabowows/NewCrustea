1e105e7626c8ed607a463290bd94aab098bd14a9
Prabowo Wahyu Sudarno <PRABOWOWahyu45@gmail.com>
2025-10-21 04:25:12 +0000

    tolong taruh paling atas (_for element <Card>_)

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -10,14 +10,16 @@ export default function DashboardPage() {
   return (
     <main className="grid flex-1 items-start gap-4 p-4 sm:px-6 sm:py-0 md:gap-8 lg:grid-cols-3 xl:grid-cols-3">
       <div className="grid auto-rows-max items-start gap-4 md:gap-8 lg:col-span-2">
-        <RealTimeMetrics />
+        <div className="grid gap-4 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-4">
+          <RealTimeMetrics />
+        </div>
         <HistoricalChart />
         <AlarmsTable />
         <ParameterAnalysis />
       </div>
       <div className="grid auto-rows-max items-start gap-4 md:gap-8 lg:col-span-1">
-        <AeratorControl />
         <LocationOverview />
+        <AeratorControl />
       </div>
     </main>
   );
